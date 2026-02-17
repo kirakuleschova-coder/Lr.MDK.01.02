a52258fd55a746a1ce118a0af76f5548bc8a12c5
Fa1tloy <baranovdaniil534@gmail.com>
2026-02-17 10:38:23 +0300

    Сохраняю

---
+++ ArendAvto/ArendsAvto/Avto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArendsAvto
+{
+    public class Avto
+    {
+        private string brand_;
+        private string model_;
+        private double price_;
+        private string owner_;
+
+        public string Brand
+        {
+            get { return brand_; }
+        }
+
+        public string Model
+        {
+            get { return model_; }
+        }
+
+        public double Price
+        {
+            get { return price_; }
+        }
+
+        public string Owner
+        {
+            get { return owner_; }
+        }
+        public Avto(string bre)
+        {
+
+        }
+    }
+}
---
+++ ArendAvto/ArendsAvto/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArendsAvto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArendsAvto")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b8a3795-be05-45da-9d2a-93d0c41fd710")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
